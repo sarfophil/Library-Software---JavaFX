9d621bdc092bccc2dd3e69f812d900905264a350
Komakech Godfrey Awich <koma92awich@gmail.com>
2019-10-08 12:56:23 -0500

    FXML DOCUMENTS

---
+++ src/main/java/Library/app/ui/LoginContoller.java
@@ -0,0 +1,23 @@
+package Library.app.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginContoller {
+	@FXML
+	private TextField uid;
+
+	@FXML
+	private PasswordField pass;
+
+	@FXML
+	private Button login;
+
+	@FXML
+	private void submit() {
+		
+	}
+
+}
