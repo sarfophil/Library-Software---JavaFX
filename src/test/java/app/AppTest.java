484796a4b73f03d8cdd4e10bc1d286a573579a9a
sarfophil <philsarfogh@gmail.com>
2019-10-07 22:23:27 -0500

    Merge Update

--- src/test/java/app/AppTest.java
+++ src/test/java/app/AppTest.java
@@ -1,17 +1,11 @@
 package app;
 
-import java.util.UUID;
-
 import org.junit.Test;
 
-<<<<<<< HEAD
 import Library.app.business.ControllerInterface;
-=======
-import Library.app.business.Address;
-import Library.app.business.ControllerInterface;
-import Library.app.business.LibraryMember;
->>>>>>> 7a96c30145a77d5c116ef1a1c01effcf30b2d779
 import Library.app.business.SystemController;
+
+
 
 public class AppTest {
 	@Test
@@ -19,17 +13,7 @@ public class AppTest {
 	
 	@Test
 	public void createNewLibraryMember() {
-<<<<<<< HEAD
 		 ControllerInterface controller = new SystemController();
 		 controller.allBookIds();
-=======
-		ControllerInterface controller = new SystemController();
-		
-		LibraryMember libraryMember = new LibraryMember(UUID.randomUUID().toString(), 
-				"Godfred", "Koma", "090999", new Address("1000 North","Fairfield","Iowa","zip"));
-		
-		controller.createNewLibraryMember(libraryMember);
-		
->>>>>>> 7a96c30145a77d5c116ef1a1c01effcf30b2d779
 	}
 }
