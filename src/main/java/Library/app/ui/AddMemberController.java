025ec26585998cb91c6b75a452039d9ccbd220df
thierry.manzi <thierry.manzi001@gmail.com>
2019-10-08 13:22:06 -0500

    Ui Screens

---
+++ src/main/java/Library/app/ui/AddMemberController.java
@@ -0,0 +1,53 @@
+package Library.app.ui;
+
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AddMemberController {
+	@FXML
+	private TextField memberId;
+	
+	@FXML
+	private TextField firstname;
+	
+	@FXML
+	private TextField lastname;
+	
+	@FXML
+	private TextField street;
+	
+	@FXML
+	private TextField city;
+	
+	@FXML
+	private TextField state;
+	
+	@FXML
+	private TextField zip;
+	
+	@FXML
+	private TextField phonenumber;
+	
+	@FXML
+	private Button addButton;
+	
+	@FXML
+	private Button backButton;
+	
+	
+	@FXML
+	private void add() {
+		
+	}
+	
+	@FXML
+	private void back() {
+		
+	}
+	
+	
+
+}
