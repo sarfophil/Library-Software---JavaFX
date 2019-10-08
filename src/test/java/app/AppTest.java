cce9a63d95612d5d113d706a3ad01456718c158b
sarfophil <philsarfogh@gmail.com>
2019-10-07 22:14:36 -0500

    Tweaks

--- src/test/java/app/AppTest.java
+++ src/test/java/app/AppTest.java
@@ -2,12 +2,16 @@ package app;
 
 import org.junit.Test;
 
+import Library.app.business.ControllerInterface;
+import Library.app.business.SystemController;
+
 public class AppTest {
 	@Test
 	public void run() {}
 	
 	@Test
 	public void createNewLibraryMember() {
-		
+		 ControllerInterface controller = new SystemController();
+		 controller.allBookIds();
 	}
 }
